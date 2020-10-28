878882fbdfbdf2b5e51d83d51117b1802db4f122
Vitaliy Slepakov <v.slepakov@gmail.com>
2020-10-28 23:29:44 +0100

    minor refactoring

--- function/MetricsFromatter/MetricsFromatter.cs
+++ function/MetricsFromatter/MetricsFromatter.cs
@@ -9,7 +9,7 @@ using Azure.Storage.Blobs.Models;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace MetricsFromatter
 {
@@ -18,8 +18,6 @@ namespace MetricsFromatter
         [FunctionName("MetricsFromatter")]
         public static async Task Run([EventHubTrigger("opc-messages", Connection = "EventHubConnectionString", ConsumerGroup ="metrics")] EventData[] events, ILogger log)
         {
-            var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-            var containerName = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONTAINER_NAME");
             var exceptions = new List<Exception>();
 
             foreach (EventData eventData in events)
@@ -27,22 +25,21 @@ namespace MetricsFromatter
                 try
                 {
                     string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
-                    var json = JObject.Parse(messageBody);
 
-                    log.LogInformation($"C# Event Hub trigger function processed a message: {messageBody}");
+                    log.LogInformation($"Event Hub trigger function received a message: {messageBody}");
 
-                    var sourceTimeStamp = DateTime.Parse((string)json["SourceTimestamp"]);
-                    var fileName = $"{(string)json["PartitionKey"]}/{sourceTimeStamp:yyyy/MM/dd/HH/mm}.json";
+                    // Due to Metrics Advisor Limitations:
+                    // Assumption / Pre-condition is that all relevant data points have the same timestamp
+                    var opcUaData = JsonConvert.DeserializeObject<IList<OpcUaDataPoint>>(messageBody);
 
-                    var blobServiceClient = new BlobServiceClient(connectionString);
-                    var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-                    var blobClient = containerClient.GetBlobClient(fileName);
+                    if (opcUaData.Any())
+                    {
+                        var first = opcUaData.First();
+                        var fileName = $"{first.ApplicationUri}/{first.SourceTimestamp:yyyy/MM/dd/HH/mm}.json";
+                        var metrics = CreateOutputMetrics(opcUaData.ToList(), first.SourceTimestamp);
 
-                    log.LogInformation($"Uploading metrics file: {fileName}");
-
-                    var metric = $"[{{\"date\": \"{sourceTimeStamp:yyyy-MM-ddTHH:mm:00Z}\",\"{(string)json["DisplayName"]}\": {(double)json["Value"]}}}]";
-                    using var stream = new MemoryStream(Encoding.UTF8.GetBytes(metric));
-                    await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = "application/json" });
+                        await UploadMetricsToBlobAsync(fileName, metrics, log);
+                    }
                 }
                 catch (Exception e)
                 {
@@ -59,6 +56,29 @@ namespace MetricsFromatter
 
             if (exceptions.Count == 1)
                 throw exceptions.Single();
+        }
+
+        private static async Task UploadMetricsToBlobAsync(string fileName, string metrics, ILogger log)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            var containerName = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONTAINER_NAME");
+
+            var blobClientOptions = new BlobClientOptions();
+            blobClientOptions.Retry.MaxRetries = 10;
+            var blobServiceClient = new BlobServiceClient(connectionString, blobClientOptions);
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            log.LogInformation($"Uploading metrics file: {fileName}");
+
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(metrics));
+            await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = "application/json" });
+        }
+
+        private static string CreateOutputMetrics(IList<OpcUaDataPoint> allData, DateTime timestamp)
+        {
+            var metrics = allData.Select(data => $"\"{data.DisplayName}\": {data.Value}");
+            return $"[{{\"date\": \"{timestamp:yyyy-MM-ddTHH:mm:00Z}\", {string.Join(',', metrics)}}}]";
         }
     }
 }
---
+++ function/MetricsFromatter/OpcUaDataPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MetricsFromatter
+{
+    public class OpcUaDataPoint
+    {
+        public string NodeId { get; set; }
+
+        public double Value { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string ApplicationUri { get; set; }
+
+        public DateTime SourceTimestamp { get; set; }
+    }
+}
